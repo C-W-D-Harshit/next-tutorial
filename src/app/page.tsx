67bd81623dbbe895537205fc69ab71de1e6df44b
Harshit Sharma <cwd.harshit911@gmail.com>
2024-01-09 23:03:00 +0530

    sdsdf

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="w-full h-screen flex justify-center items-center">
+    <main className="w-full flex justify-center items-center">
       <h1 className="font-bold text-2xl">Hi Boss!</h1>
     </main>
   );
